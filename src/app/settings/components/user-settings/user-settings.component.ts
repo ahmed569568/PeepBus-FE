6a0d1ec2e1232701ec58fcfc2d888874c32cd92f
kareem arafa <k4ho@live.com>
2019-10-13 21:43:01 +0300

    fix user settings issues

--- src/app/settings/components/user-settings/user-settings.component.ts
+++ src/app/settings/components/user-settings/user-settings.component.ts
@@ -12,7 +12,7 @@ import { MapService } from '@app/shared/services/map.service';
 export class UserSettingsComponent implements OnInit {
   imageSrc: string | ArrayBuffer;
   form: FormGroup;
-  imageFieldName: string;
+  imageFieldName: any[] = [];
   userID: number;
 
   lat = 51.678418;
@@ -36,6 +36,7 @@ export class UserSettingsComponent implements OnInit {
     this.userID = this.credentials.credentials ? this.credentials.credentials.id : null;
     this.initForm();
     this.fetchAddress();
+    this.loadResources(1);
   }
 
   fetchAddress() {
@@ -48,6 +49,19 @@ export class UserSettingsComponent implements OnInit {
     });
   }
 
+  /**
+   * fetch single item data from service
+   * and fill form with it in Edit forms
+   */
+  loadResources(id: number): void {
+    this.api.get(`clientsetting/${id}/show`).subscribe(response => {
+      const obj = JSON.parse(JSON.stringify(response.response)); // clone response object
+      this.form.controls.address.setValue(obj.location.address);
+      this.form.controls.location_id.setValue(obj.location.id);
+      return this.form.patchValue(obj);
+    });
+  }
+
   initForm() {
     this.form = this.fb.group({
       school_name: [''],
@@ -57,12 +71,13 @@ export class UserSettingsComponent implements OnInit {
       phone: [''],
       address: [{ value: '', disabled: true }],
       lat: [''],
-      lng: ['']
+      lng: [''],
+      location_id: ['']
     });
   }
 
   uploadPhoto(event: any, field: string) {
-    this.imageFieldName = field;
+    this.imageFieldName.push(field);
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
@@ -73,22 +88,44 @@ export class UserSettingsComponent implements OnInit {
 
   convertImage(readerEvt: any) {
     const binaryString = readerEvt.target.result;
-    if (this.imageFieldName) {
-      this.imagePath(null, btoa(binaryString));
-    }
-    this.form.controls[this.imageFieldName].setValue(btoa(binaryString));
+    return this.setImagesValue(btoa(binaryString));
   }
 
-  imagePath(prefix: string, value: any) {
+  setImagesValue(base64: string) {
+    if (Object.keys(this.imageFieldName)) {
+      this.imagePath(null, base64);
+    }
+    for (const fieldName of this.imageFieldName) {
+      this.form.controls[fieldName].setValue(this.imagePath(fieldName, base64));
+    }
+  }
+
+  imagePath(prefix: string, value: any, preview?: boolean) {
     if (value.length < 100) {
-      return (this.imageSrc = prefix + value);
+      return prefix + value;
     } else {
-      return (this.imageSrc = 'data:image/png;base64,' + value);
+      if (preview) {
+        return value;
+      } else {
+        return 'data:image/png;base64,' + value;
+      }
     }
   }
 
   submit() {
-    console.log(this.form.value);
-    this.api.post(`clientsetting/${this.userID}/update`).subscribe(resp => console.log(resp));
+    const form = {
+      school_name: this.form.controls.school_name.value,
+      school_email: this.form.controls.school_email.value,
+      website: this.form.controls.website.value,
+      image: this.form.controls.image.value,
+      phone: this.form.controls.phone.value,
+      location: {
+        id: this.form.controls.location_id.value,
+        address: this.form.controls.address.value,
+        lat: this.form.controls.lat.value.toString(),
+        lng: this.form.controls.lng.value.toString()
+      }
+    };
+    this.api.put(`clientsetting/1/update`, form).subscribe(resp => console.log(resp));
   }
 }
